aef9b0cef790c1813a5379b5b9dcbffe68abe72c
james <jamesdesbyrne@gmail.com>
2015-04-24 22:35:12 +0100

    Added the MyMap functionality into the main class
    Fixed a bug that would have caused the robot to spin endlessly

--- CA2/Main.java
+++ CA2/Main.java
@@ -1,6 +1,6 @@
 /*
-Team 1 
-Name      Student Number 
+Team 1
+Name      Student Number
 - James Byrne   C11393906
 - Juozas Dapkunas   C12432832
 */
@@ -13,28 +13,34 @@ import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class Main{
-    
+
     public static void main(String [] args) {
-      
-      Button.ESCAPE.addButtonListener(new ButtonListener() {
-      public void buttonPressed(Button b) {
-         System.exit(0);
-       }
 
-      public void buttonReleased(Button b) {
-         LCD.clear();
-      }
-    });
+        Button.ESCAPE.addButtonListener(new ButtonListener() {
+            public void buttonPressed(Button b) {
+                System.exit(0);
+            }
+
+            public void buttonReleased(Button b) {
+                LCD.clear();
+            }
+        });
 
 
-    Behavior carpet = new CarpetSensor();
-    Behavior collision = new Touch();
-    Behavior vacuum = new Sonar();
-    Behavior [] bArray = {collision, carpet, vacuum};
-    Arbitrator arby = new Arbitrator(bArray);
-    arby.start();
+        Behavior carpet = new CarpetSensor();
+        Behavior collision = new Touch();
+        Behavior vacuum = new Sonar();
+        Behavior [] bArray = {collision, carpet, vacuum};
+        Arbitrator arby = new Arbitrator(bArray);
+        arby.start();
 
+        // Creates a new instance of the MyMap class
+        MyMap map = new MyMap();
+        // Holds the logic for getting the map of the room
+        // i.e. its dimensions in units
+        map.getOutline();
+        // "Fills In" the map that was plotted beforehand 
+        map.fillIn();
     }
 
 }
-
--- CA2/MyMap.java
+++ CA2/MyMap.java
@@ -9,15 +9,14 @@ import lejos.nxt.*;
 
 public class MyMap extends Thread {
 
-    private int myX;
-    private int myY;
+
     private int END_MAP=4;
     static int[] matrice;
     public static Movement m;
-
+    // Used to count the number of times sonar interrupts
     public static int sonar=0;
 
-    // Moving within the  map
+    // Default Constructor
     public void MyMap() {
 
     }
@@ -50,11 +49,26 @@ public class MyMap extends Thread {
         // Get the width and lenght of the area to be filled in
         float X_LIMIT = matrice[0];
         float Y_LIMIT = matrice[1];
+        //Measure the current Y value and create a switch for the
+        // robots turn logic
         float curr_Y = 0;
-
+        int i = 0;
+        // While the robot has not traveled n units in the Y plane
+        // continue to move in the X plane until a limit is reached
+        // then arc to move in the direction of positive y
         while(curr_Y < Y_LIMIT){
+            // Move to near the edge
             m.forward(X_LIMIT-7);
-            m.arcRight(3.5f, 180);
+            // If the last turn was a left turn then turn right
+            if (i == 0){
+                m.arcRight(3.5f, 180);
+            }
+            // If the last turn was a right turn then turn left
+            else {
+                i = 0;
+                m.arcLeft(3.5f, 180);
+            }
+            // Update the Y value 
             curr_Y += 3.5;
         }
     }
