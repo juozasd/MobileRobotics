5729afa38429320a9cf74c195e97ecd90f9a2c56
juozasd <joethetall@gmail.com>
2015-04-24 17:37:59 +0100

    Merge Error Fix

--- CA2/Touch.java
+++ CA2/Touch.java
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-=======
 /*
 Team 1 
 Name      Student Number 
 - James Byrne   C11393906
 - Juozas Dapkunas   C12432832
 */
-
->>>>>>> 9b68aab309902266115f4b331c64acc85c4d0de1
 import lejos.nxt.*;
 import lejos.robotics.subsumption.*;
 
@@ -17,13 +13,8 @@ public class Touch extends Thread implements Behavior {
         private TouchSensor touch;
         private boolean suppreseed = false;
 
-<<<<<<< HEAD
-        public touch(SensorPort port){
-            sonar = new UltrasonicSensor(port);
-=======
         public Touch(){
                 touch = new TouchSensor(SensorPort.S4);
->>>>>>> 9b68aab309902266115f4b331c64acc85c4d0de1
         }
 
         public boolean takeControl() {
@@ -38,22 +29,11 @@ public class Touch extends Thread implements Behavior {
 
         public void action()
         {
-<<<<<<< HEAD
-            Movement movement = new Movement();
-	    // make room to arc
-            movement.backward(10);
-		    float arc = (3.14 * 10);
-	    // do an arc around the object
-            movement.arRight(arc, 180);
-        }
-=======
-                Movement movement = new Movement();
-                
+            Movement movement = new Movement();        
 		// make room to arc
-                movement.backward(10);
-		float arc = (3.14f * 10f);
+            movement.backward(10);
+		    float arc = (3.14f * 10f);
 		// do an arc around the object
-		movement.arcRight(arc, 45);
+		    movement.arcRight(arc, 45);
         }                                            
 }
->>>>>>> 9b68aab309902266115f4b331c64acc85c4d0de1
